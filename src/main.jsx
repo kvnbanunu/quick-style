028b0a84f57c15e53bf86b66b6f91a45f368b134
kvnbanunu <kvnbanunu@gmail.com>
2026-03-14 11:39:06 -0700

    fix: moved components into app

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import QuickStyle from "./components/QuickStyle.jsx";
+import QuickStyle from "./components/QuickStyle";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QuickStyle />
-    <div>
-      Test Element
-      <div>
-        Child Element
-        <p>Paragraph</p>
-      </div>
-    </div>
     <App />
   </StrictMode>,
 );
