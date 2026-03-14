ed9aeb42a4bfa1ad03c7a7c8e2573d35f1308c5e
unknown <lpeder@shaw.ca>
2026-03-13 20:50:26 -0700

    implemented writing classes from the datalist and adding to a list of classes that will be on an element

--- src/components/QuickStyle.jsx
+++ src/components/QuickStyle.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ElementEditor from "./elementEditor";
 
 export default function QuickStyle() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,7 +10,10 @@ export default function QuickStyle() {
         id="quickstyle-editor"
         className="border border-1 bg-black w-md h-md min-h-48 z-10 rounded absolute bottom-10 right-10 flex flex-col justify-between"
       >
-        <p className="text-lg">Quick Style Editor</p>
+        <p className="text-lg">
+          Quick Style Editor
+          <ElementEditor/>
+          </p>
         <button
           onClick={() => {
             setIsOpen(false);
---
+++ src/components/elementEditor.jsx
@@ -0,0 +1,79 @@
+export default function ElementEditor() {
+
+  return (
+    <div>
+      <ClassInput/>
+    </div>
+  )
+}
+
+
+const addedClasses = [];
+
+const classes = [
+  "bg-red-500",
+  "bg-blue-500",
+  "bg-green-500",
+  "bg-purple-500",
+  "bg-orange-500",
+
+  "text-red-500",
+  "text-blue-500",
+  "text-green-500",
+  "text-purple-500",
+
+  "p-1",
+  "p-2",
+  "p-3",
+  "p-4",
+  "p-6",
+  "p-8",
+
+  "px-2",
+  "px-4",
+  "px-6",
+
+  "py-2",
+  "py-4",
+  "py-6",
+
+  "rounded",
+  "rounded-md",
+  "rounded-lg",
+  "rounded-xl",
+
+  "flex",
+  "grid",
+  "items-center",
+  "justify-center"
+];
+
+function addClass(cls) {
+  console.log("adding class");
+  addedClasses[addedClasses.length] = cls;  
+  console.log(addedClasses);
+}
+
+function ClassInput() {
+  return (
+    <div>
+      <input 
+      placeholder="add a class..."
+      list="class-list"
+      id="country-choice" 
+      name="country-choice" 
+      onKeyDown={(e) => {
+        if(e.key === "Enter") {
+          addClass(e.target.value.trim())
+          e.target.value = ""
+        }
+      }}
+      />
+      <datalist id="class-list">
+        {classes.map((classes) => (
+          <option key={classes} value={classes} />
+        ))}
+      </datalist>
+    </div>
+  );
+}
